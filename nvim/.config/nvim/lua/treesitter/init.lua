217aee5950dfde6fec7389ab2e993c5a27217bf4
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-05-06 10:13:06 +0545

    Updated the local variable `pattern` in ~/.config/nvim/lua/treesitter/init.lua to`filetypes` for proper representation

--- nvim/.config/nvim/lua/treesitter/init.lua
+++ nvim/.config/nvim/lua/treesitter/init.lua
@@ -1,5 +1,5 @@
--- Filetypes for which to enable Treesitter highlighting and folding
-local pattern = {'lua','cpp','python','js','javascript','typescript','javascriptreact','tsx','html','css','rust','go','sh','markdown','yaml','toml'}
+-- Filetypes(associated with diff. lang.) for which to enable Treesitter highlighting and folding
+local filetypes = {'lua','cpp','python','js','javascript','typescript','javascriptreact','tsx','html','css','rust','go','sh','markdown','yaml','toml'}
 
 -- Autocommand to start Treesitter highlighting and folding for specific filetypes using vim.treesitter APIs
 local treesitter_group = vim.api.nvim_create_augroup('TreesitterGroup', { clear = true })
