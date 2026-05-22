baf7e1b5ccb8da59d5e88cbf202e74a07a7b68a4
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-05-22 15:30:07 +0545

    update: updated the workspace selector for the 'on-scroll-up' and 'on-scroll-down' options for the `hyprland/workspaces` module in waybar

--- hypr/.config/hypr/scripts/suspend.sh
+++ hypr/.config/hypr/scripts/suspend.sh
@@ -2,4 +2,5 @@
 
 playerctl -a pause
 hyprctl dispatch "hl.dsp.exec_cmd('hyprlock')"
+sleep 2
 systemctl suspend
--- nvim/.config/nvim/lua/lspConfig/init.lua
+++ nvim/.config/nvim/lua/lspConfig/init.lua
@@ -1,5 +1,5 @@
 -- NOTE: use conform.nvim & nvim-lint for formatters and linters that don't have an LSP server
-vim.lsp.enable({ 'lua_ls', 'ts_ls', 'tailwindcss', 'pyright', 'clangd', 'bashls' })
+vim.lsp.enable({ 'lua_ls', 'ts_ls', 'tailwindcss', 'pyright', 'clangd', 'bashls', 'copilot' })
 
 -- Adding .git as a guaranteed fallback root marker, for all servers
 vim.lsp.config('*', {
