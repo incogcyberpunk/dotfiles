399946abdc15888d1410dd40bd0b2e9003bc8e0b
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-06-08 19:05:17 +0545

    style: improve formatting for autostart.lua using the stylua formatter

--- hypr/.config/hypr/conf/autostart.lua
+++ hypr/.config/hypr/conf/autostart.lua
@@ -3,40 +3,40 @@
 
 -- Using `hl.on` to define an event listener for the event `hyprland.start`
 hl.on("hyprland.start", function()
-    -- Using waypaper(wallpaper setter) and awww(wayland wallpaper daemon)
-    hl.exec_cmd("awww-daemon")
+	-- Using waypaper(wallpaper setter) and awww(wayland wallpaper daemon)
+	hl.exec_cmd("awww-daemon")
 
-    -- Startup waybar on startup
-    hl.exec_cmd("waybar")
+	-- Startup waybar on startup
+	hl.exec_cmd("waybar")
 
-    -- Udiskie
-    hl.exec_cmd("udiskie")
+	-- Udiskie
+	hl.exec_cmd("udiskie")
 
-    -- Load Notification Daemon
-    hl.exec_cmd("swaync")
+	-- Load Notification Daemon
+	hl.exec_cmd("swaync")
 
-    -- Launch hyprsunset and hypridle
-    hl.exec_cmd("hyprsunset")
-    hl.exec_cmd("hypridle")
+	-- Launch hyprsunset and hypridle
+	hl.exec_cmd("hyprsunset")
+	hl.exec_cmd("hypridle")
 
-    -- Setup cliphist to store images and text
-    hl.exec_cmd("wl-paste --type text --watch cliphist store")
-    hl.exec_cmd("wl-paste --type image --watch cliphist store")
+	-- Setup cliphist to store images and text
+	hl.exec_cmd("wl-paste --type text --watch cliphist store")
+	hl.exec_cmd("wl-paste --type image --watch cliphist store")
 
-    -- Launch discord silently on workspace 15
-    hl.exec_cmd("discord", { workspace = 15 })
+	-- Launch discord silently on workspace 15
+	hl.exec_cmd("discord", { workspace = 15 })
 
-    -- Execute the tmux script
-    hl.exec_cmd("~/.config/hypr/scripts/tmuxSetup", { workspace = 3 })
+	-- Execute the tmux script
+	hl.exec_cmd("~/.config/hypr/scripts/tmuxSetup", { workspace = 3 })
 
-    -- Load the nepali date
-    hl.exec_cmd("~/sysScripts/fetchNepaliDate.sh")
+	-- Load the nepali date
+	hl.exec_cmd("~/sysScripts/fetchNepaliDate.sh")
 end)
 
 hl.on("workspace.move_to_monitor", function(ws, m)
-    hl.notification.create({
-        text = "Workspace: " .. ws.name .. " moved to a monitor at x: " .. m.position.x,
-        timeout = 4000,
-        icon = "ok"
-    })
+	hl.notification.create({
+		text = "Workspace: " .. ws.name .. " moved to a monitor at x: " .. m.position.x,
+		timeout = 4000,
+		icon = "ok",
+	})
 end)
