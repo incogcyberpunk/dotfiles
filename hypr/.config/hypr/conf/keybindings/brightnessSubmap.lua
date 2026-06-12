54bdea8020efd2e3de02c2ac6be8ac44cfef6ac3
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-06-12 08:19:25 +0545

    refactor: refactor submap using hl.dispatch and lua function for dispatcher to keybind

--- hypr/.config/hypr/conf/keybindings/brightnessSubmap.lua
+++ hypr/.config/hypr/conf/keybindings/brightnessSubmap.lua
@@ -1,27 +1,40 @@
 local scripts = "~/.config/hypr/scripts"
 
 hl.define_submap("changeBrightness", function()
-    hl.bind("J", hl.dsp.exec_cmd(scripts .. "/changeBrightness.sh decrease"), { repeating = true })
-    hl.bind("K", hl.dsp.exec_cmd(scripts .. "/changeBrightness.sh increase"), { repeating = true })
+	hl.bind("J", hl.dsp.exec_cmd(scripts .. "/changeBrightness.sh decrease"), { repeating = true })
+	hl.bind("K", hl.dsp.exec_cmd(scripts .. "/changeBrightness.sh increase"), { repeating = true })
 
-    hl.bind("Q",
-        hl.dsp.exec_cmd(
-            "notify-send -t 900 -h string:x-canonical-private-synchronous:brightness-submap 'Exited Brightness Submap'"),
-        { locked = true })
-    hl.bind("Q", hl.dsp.submap("reset"), { locked = true })
-    hl.bind("ESCAPE",
-        hl.dsp.exec_cmd(
-            "notify-send -t 900 -h string:x-canonical-private-synchronous:brightness-submap 'Exited Brightness Submap'"),
-        { locked = true })
-    hl.bind("ESCAPE", hl.dsp.submap("reset"), { locked = true })
-    hl.bind("catchall",
-        hl.dsp.exec_cmd(
-            "notify-send -t 900 -h string:x-canonical-private-synchronous:brightness-submap 'Exited Brightness Submap' && hyprctl dispatch submap reset"),
-        { locked = true })
+	hl.bind("Q", function()
+		hl.dispatch(
+			hl.dsp.exec_cmd(
+				"notify-send -t 900 -h string:x-canonical-private-synchronous:audio-submap 'Exited Brightness Submap' "
+			)
+		)
+		hl.dispatch(hl.dsp.submap("reset"))
+	end, { locked = true })
+	hl.bind("ESCAPE", function()
+		hl.dispatch(
+			hl.dsp.exec_cmd(
+				"notify-send -t 900 -h string:x-canonical-private-synchronous:audio-submap 'Exited Brightness Submap' "
+			)
+		)
+		hl.dispatch(hl.dsp.submap("reset"))
+	end, { locked = true })
+	-- hl.bind("catchall", function()
+	-- 	hl.dispatch(
+	-- 		hl.dsp.exec_cmd(
+	-- 			"notify-send -t 900 -h string:x-canonical-private-synchronous:audio-submap 'Exited Brightness Submap' "
+	-- 		)
+	-- 	)
+	-- 	hl.dispatch(hl.dsp.submap("reset"))
+	-- end, { locked = true })
 end)
 
-hl.bind("SUPER + B",
-    hl.dsp.exec_cmd(
-        "notify-send -u low -h string:x-canonical-private-synchronous:brightness-submap 'Entering Brightness Submap'"),
-    { locked = true })
+hl.bind(
+	"SUPER + B",
+	hl.dsp.exec_cmd(
+		"notify-send -u low -h string:x-canonical-private-synchronous:brightness-submap 'Entering Brightness Submap'"
+	),
+	{ locked = true }
+)
 hl.bind("SUPER + B", hl.dsp.submap("changeBrightness"), { locked = true })
