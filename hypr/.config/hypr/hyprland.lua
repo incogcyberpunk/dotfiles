a5179bde3907d356e1375d69226c68b167f006b2
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-06-19 17:16:12 +0545

    refactor: update formatting

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -6,48 +6,48 @@
 --         |___/|_|
 
 -- Monitor
-require('monitors')
+require("monitors")
 
 -- Workspace Rules
-require('workspaces')
+require("workspaces")
 
 -- Cursor
-require('conf.cursor')
+require("conf.cursor")
 
 -- Touchpad
-require('conf.touchpad')
+require("conf.touchpad")
 
 -- Environment
-require('conf.environment')
+require("conf.environment")
 
 -- Autostart
-require('conf.autostart')
+require("conf.autostart")
 
 -- Load configuration files
-require('conf.decoration')
-require('conf.variables')
-require('conf.layout')
-require('conf.misc')
-require('conf.windowrule')
-require('conf.keybindings.appKeybinds')
-require('conf.keybindings.audioKeybinds')
-require('conf.keybindings.audioSubmap')
-require('conf.keybindings.brightnessSubmap')
-require('conf.keybindings.default')
-require('conf.keybindings.disableKeybinds')
-require('conf.keybindings.fnKeybinds')
-require('conf.keybindings.hyprshade')
-require('conf.keybindings.laptopLid')
-require('conf.keybindings.screenshot')
-require('conf.keybindings.utilites')
-require('conf.keybindings.windowSubmap')
-require('conf.keybindings.workspaceKeybinds')
+require("conf.decoration")
+require("conf.variables")
+require("conf.layout")
+require("conf.misc")
+require("conf.windowrule")
+require("conf.keybindings.appKeybinds")
+require("conf.keybindings.audioKeybinds")
+require("conf.keybindings.audioSubmap")
+require("conf.keybindings.brightnessSubmap")
+require("conf.keybindings.default")
+require("conf.keybindings.disableKeybinds")
+require("conf.keybindings.fnKeybinds")
+require("conf.keybindings.hyprshade")
+require("conf.keybindings.laptopLid")
+require("conf.keybindings.screenshot")
+require("conf.keybindings.utilites")
+require("conf.keybindings.windowSubmap")
+require("conf.keybindings.workspaceKeybinds")
 
 -- Load animations
-require('conf.animation')
+require("conf.animation")
 
 -- Permissions
-require('conf.permissions')
+require("conf.permissions")
 
 -- Gestures
-require('conf.gestures')
+require("conf.gestures")
