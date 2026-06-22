18a103ee6695ae71238702bd0539b1e53003d3e0
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-06-22 07:52:12 +0545

    feat(bit-depth): change bit-depth of displays from default value of 8 to 10
    Previously, the bit-depth was set to default value of 8, while both the
    displays supported upto bit-depth=12, but Hyprland only allows for value
    of 8 or 10. So, update the bit-depth to max. allowed by Hyprland i.e. 10

--- hypr/.config/hypr/monitors.lua
+++ hypr/.config/hypr/monitors.lua
@@ -1,17 +1,16 @@
-
 hl.monitor({
-    output = "eDP-1",
-    mode = "1920x1080",
-    position = "0x0",
-    scale = '1',
+	output = "eDP-1",
+	mode = "1920x1080",
+	position = "0x0",
+	scale = "1",
+	bitdepth = 10,
 })
 hl.monitor({
-    output = "HDMI-A-1",
-    mode = "preferred",
-    position = "1920x0",
-    scale = '1',
+	output = "HDMI-A-1",
+	mode = "preferred",
+	position = "1920x0",
+	scale = "1",
+	bitdepth = 10,
 })
 -- Rule for quickly plugging in random monitors
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "1" })
-
-
