24c28c521a96144ed94bfa97b0c687e86233402c
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-07-04 12:45:47 +0545

    feat(border-color): update active window border color to '#89b4fa'

--- hypr/.config/hypr/conf/variables.lua
+++ hypr/.config/hypr/conf/variables.lua
@@ -1,14 +1,17 @@
 hl.config({
-    general  = {
-        gaps_in = 1.5,
-        gaps_out = 0,
-    },
-    binds    = {
-        allow_workspace_cycles = true,
-        workspace_back_and_forth = true,
-        workspace_center_on = 1,
-    },
-    xwayland = {
-        enabled = true,
-    }
+	general = {
+		gaps_in = 1.5,
+		gaps_out = 0,
+		col = {
+			active_border = "#89b4fa",
+		},
+	},
+	binds = {
+		allow_workspace_cycles = true,
+		workspace_back_and_forth = true,
+		workspace_center_on = 1,
+	},
+	xwayland = {
+		enabled = true,
+	},
 })
