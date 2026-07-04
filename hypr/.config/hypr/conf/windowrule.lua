18727060d2cec4e01fd2f697c96980337123309f
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-07-04 11:46:13 +0545

    feat(nwg-displays): add hyprland window rules to open nwg-displays in float window

--- hypr/.config/hypr/conf/windowrule.lua
+++ hypr/.config/hypr/conf/windowrule.lua
@@ -1,2 +1,13 @@
 -- WINDOW RULES
--- https://wiki.hypr.land/Configuring/Basics/Window-Rules/
+-- https://wiki.hypr.land/0.55.0/Configuring/Basics/Window-Rules/
+
+-- Float nwg-displays window
+hl.window_rule({
+	name = "nwg-displays-float",
+	match = {
+		class = "nwg-displays",
+		title = "nwg-displays",
+	},
+	float = true,
+	size = { 1200, 600 },
+})
