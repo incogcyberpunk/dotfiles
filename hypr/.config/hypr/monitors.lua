81a151ab08b507aa6dca3005beaaaee089681f2f
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-07-10 11:27:36 +0545

    fix: fix incorrect mode in monitors' conf.

--- hypr/.config/hypr/monitors.lua
+++ hypr/.config/hypr/monitors.lua
@@ -1,15 +1,17 @@
 
 hl.monitor({
     output = "eDP-1",
-    mode = "1920x1080",
+    mode = "preferred",
     position = "0x0",
     scale = '1',
+    bitdepth = 10,
 })
 hl.monitor({
     output = "HDMI-A-1",
     mode = "preferred",
     position = "1920x0",
     scale = '1',
+    bitdepth = 10,
 })
 -- Rule for quickly plugging in random monitors
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "1" })
