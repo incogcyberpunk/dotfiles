c1de2063240a4d6cc353f36821e67a92e79f71f7
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-07-27 08:22:41 +0545

    feat: add a rule to mirror any new display when only laptop display is enabled

--- hypr/.config/hypr/scripts/laptopDisplayEnable.sh
+++ hypr/.config/hypr/scripts/laptopDisplayEnable.sh
@@ -12,6 +12,8 @@ hl.monitor({
     output = \"HDMI-A-1\",
     disabled = true
 })
+-- Rule for quickly plugging in random monitors
+hl.monitor({ output = \"\", mode = \"preferred\", position = \"auto\", scale = \"1\" , mirror = \"eDP-1\"})
 "
 if echo -e "$config" > ~/.config/hypr/monitors.lua ; then
     notify-send "Enabled display of Laptop"
