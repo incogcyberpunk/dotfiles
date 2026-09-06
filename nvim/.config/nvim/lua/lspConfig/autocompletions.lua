a69531c4abac02f3367f7e34acb0d149bb2805a9
IncogCyberpunk <incogcyberpunk@gmail.com>
2026-09-06 23:33:08 +0545

    cleanup: remove duplicate bindings for inlay hints' toggle

--- nvim/.config/nvim/lua/lspConfig/autocompletions.lua
+++ nvim/.config/nvim/lua/lspConfig/autocompletions.lua
@@ -2,9 +2,4 @@ vim.opt.completeopt = 'menuone,noselect,popup'
 
 vim.opt.winborder = 'rounded'
 
--- Inlay hints toggle (not a default keymap)
-vim.keymap.set('n', '<leader>it', function()
-    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-end, { remap = false, desc = 'Toggle Inlay Hints' })
-
 -- Completions handled by blink.cmp
